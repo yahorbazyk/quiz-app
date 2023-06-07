c9eb01d21e85a74fa18db943a109212f1280017a
Yahor Bazyk <yahor.bazyk@sungagefinancial.com>
2023-06-07 06:35:29 +0300

    modified

--- src/QuizApp/QuizApp.js
+++ src/QuizApp/QuizApp.js
@@ -1,31 +1,55 @@
-import React, { useState } from 'react';
-import questionsData from '../data//questions';
+import React, { useState, useCallback, memo } from 'react';
+import questionsData from '../data/questions';
 import './QuizApp.css';
+
+const Question = memo(({ question, currentQuestionIndex, selectedAnswer, handleAnswerClick }) => (
+  <div>
+    <h1>
+      {currentQuestionIndex + 1}. {question.question}
+    </h1>
+    <ul className="questins">
+      {question.answers.map((answer, index) => (
+        <li key={answer.id}>
+          <button
+            onClick={() => handleAnswerClick(index)}
+            disabled={selectedAnswer !== null}
+            className={selectedAnswer !== null && (index === question.correctAnswer ? 'correct' : 'incorrect')}
+          >
+            {index + 1}. {answer}
+          </button>
+        </li>
+      ))}
+    </ul>
+  </div>
+));
 
 function QuizApp() {
   const [questions, setQuestions] = useState(questionsData.questions);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [answers, setAnswers] = useState(Array(questionsData.questions.length).fill(null));
 
-  const handleAnswerClick = (answerIndex) => {
-    setAnswers((prev) => {
-      const newAnswers = [...prev];
-      newAnswers[currentQuestionIndex] = answerIndex;
-      return newAnswers;
-    });
-  };
+  const handleAnswerClick = useCallback(
+    (answerIndex) => {
+      setAnswers((prev) => {
+        const newAnswers = [...prev];
+        newAnswers[currentQuestionIndex] = answerIndex;
+        return newAnswers;
+      });
+    },
+    [currentQuestionIndex]
+  );
 
-  const goToNextQuestion = () => {
+  const goToNextQuestion = useCallback(() => {
     setCurrentQuestionIndex((prev) => Math.min(prev + 1, questions.length - 1));
-  };
+  }, [questions]);
 
-  const goToPrevQuestion = () => {
+  const goToPrevQuestion = useCallback(() => {
     setCurrentQuestionIndex((prev) => Math.max(prev - 1, 0));
-  };
+  }, []);
 
-  const resetQuiz = () => {
+  const resetQuiz = useCallback(() => {
     setAnswers(Array(questionsData.questions.length).fill(null));
-  };
+  }, []);
 
   const currentQuestion = questions[currentQuestionIndex];
   const selectedAnswer = answers[currentQuestionIndex];
@@ -33,29 +57,13 @@ function QuizApp() {
   return (
     <div className="container">
       <div className="quiz-container">
-        <h1>
-          {currentQuestionIndex + 1}. {currentQuestion.question}
-        </h1>
-        <br />
-        <ul className="questins">
-          {currentQuestion.answers.map((answer, index) => (
-            <li key={index}>
-              <button
-                onClick={() => handleAnswerClick(index)}
-                disabled={selectedAnswer !== null}
-                className={selectedAnswer !== null && (index === currentQuestion.correctAnswer ? 'correct' : 'incorrect')}
-              >
-                {index + 1}. {answer}
-              </button>
-            </li>
-          ))}
-        </ul>
-        {selectedAnswer !== null && (
-          <div>
-            <h3>{selectedAnswer === currentQuestion.correctAnswer ? 'Верно!' : 'Неверно!'}</h3>
-          </div>
-        )}
-        <br />
+        <Question
+          question={currentQuestion}
+          currentQuestionIndex={currentQuestionIndex}
+          selectedAnswer={selectedAnswer}
+          handleAnswerClick={handleAnswerClick}
+        />
+        {selectedAnswer !== null && <h3>{selectedAnswer === currentQuestion.correctAnswer ? 'Верно!' : 'Неверно!'}</h3>}
         <br />
         <div>
           <button onClick={goToPrevQuestion} disabled={currentQuestionIndex === 0}>
